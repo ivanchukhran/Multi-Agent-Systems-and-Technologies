2396d6ee3f6dece5a37b139802a9daee1eed7b03
Ivan Chukhran <ivan.chuhran@gmail.com>
2025-04-02 00:47:09 +0300

    add agent arguments

--- multi-agent-systems/012/src/main/java/nure/labs/mast/BookBuyerAgent.java
+++ multi-agent-systems/012/src/main/java/nure/labs/mast/BookBuyerAgent.java
@@ -1,12 +1,35 @@
 package nure.labs.mast;
 
+import jade.core.AID;
 import jade.core.Agent;
 
 public class BookBuyerAgent extends Agent {
 
+    private String targetBookTitle;
+
+    private AID[] sellerAgents = {
+        new AID("seller1", AID.ISLOCALNAME),
+        new AID("seller2", AID.ISLOCALNAME),
+    };
+
     protected void setup() {
         System.out.println(
             "Hello! Buyer-agent " + getAID().getName() + " is ready."
+        );
+
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            targetBookTitle = (String) args[0];
+            System.out.println("Trying to buy " + targetBookTitle + "...");
+        } else {
+            System.out.println("No target book title specified.");
+            doDelete();
+        }
+    }
+
+    protected void takeDown() {
+        System.out.println(
+            "Buyer-agent " + getAID().getName() + " terminating."
         );
     }
 }
