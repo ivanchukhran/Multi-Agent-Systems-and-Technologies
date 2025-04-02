bc3e9fd141d9e80a6caa4b854dc7ab284e7bb3e2
Ivan Chukhran <ivan.chuhran@gmail.com>
2025-04-02 16:13:32 +0300

    fix window opening

--- multi-agent-systems/012/src/main/java/nure/labs/mast/BookSellerGui.java
+++ multi-agent-systems/012/src/main/java/nure/labs/mast/BookSellerGui.java
@@ -94,7 +94,6 @@ class BookSellerGui extends JFrame {
         int centerX = (int) screenSize.getWidth() / 2;
         int centerY = (int) screenSize.getHeight() / 2;
         setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
-        super.setVisible(true);
-        // super.show();
+        super.show();
     }
 }
